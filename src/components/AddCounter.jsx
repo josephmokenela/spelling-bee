d7de67237c4d82451a07a89d87b2ab69416b4fc4
Joseph Mokenela <joseph.mokenela@gmail.com>
2025-03-20 01:09:40 +0200

    Form component

---
+++ src/components/AddCounter.jsx
@@ -0,0 +1,14 @@
+export function AddCounter() {
+
+    return (
+        <>
+            <h2>Add Counter</h2>
+            <p>
+
+            </p>
+            <p>
+                
+            </p>
+        </>
+    );
+}
