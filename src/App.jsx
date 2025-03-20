d4710d33f16227c17de477260d784cacd482abb3
Joseph Mokenela <joseph.mokenela@gmail.com>
2025-03-20 19:01:27 +0200

    Stripped off everything and left with the skeleton code

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,10 @@
-import { useReducer } from "react";
-import { counterReducer } from './reducers/counterReducer';
-import { CounterObj } from './models/counterObj';
-import { tabReducer } from './reducers/tabReducer';
-import { CounterContext, CounterDispatchContext, TabContext, TabDispatchContext } from './contexts/context';
-import { CounterList } from "./components/CounterList";
-import { CounterTools } from "./components/CounterTools";
 import './App.css';
-import { AddCounter } from "./components/AddCounter";
 
 function App() {
-  const [counterData, counterDispatch] = useReducer(counterReducer, [
-    new CounterObj(1, {longName: 'Counter A', shortName: 'A'}, 1, 0),
-    new CounterObj(2, {longName: 'Counter B', shortName: 'B'}, 2, 0),
-    new CounterObj(3, {longName: 'Counter C', shortName: 'C'}, 1, 0),
-  ]);
-
-  const [visibleTab, tabDispatch] = useReducer(tabReducer, 1);
 
   return (
     <>
-    <CounterContext.Provider value={counterData}>
-      <CounterDispatchContext.Provider value={counterDispatch}>
-        <TabContext.Provider value={visibleTab}>
-          <TabDispatchContext.Provider value={tabDispatch}>
-            <h1>Counters</h1>
-            <section>
-              <AddCounter />
-            </section>
-            <section>
-              <CounterList />
-              <CounterTools />
-            </section>
-          </TabDispatchContext.Provider>
-        </TabContext.Provider>
-      </CounterDispatchContext.Provider>
-    </CounterContext.Provider>
+    
     </>
   );
 }
--- src/contexts/context.js
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-
-export const CounterContext = createContext(null);
-export const CounterDispatchContext = createContext(null);
-export const TabContext = createContext(null);
-export const TabDispatchContext = createContext(null);
